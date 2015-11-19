de07f75b70f35c059e0d80412c1aea659aa5328b
switer <guankaishe@gmail.com>
2015-11-19 23:09:00 +0800

    test case

--- test/spec.js
+++ test/spec.js
@@ -41,6 +41,57 @@ describe('Global API', function () {
         })
         assert(inited)
     })
+    it('directive-expression', function () {
+        var inited = false
+        Reve.directive('num', {
+            bind: function (v, expr) {
+                inited = true
+                assert.equal(v, 30)
+                assert.equal(expr, 'num + 20')
+            },
+            update: function (v) {
+                assert.equal(v, 30)
+            }
+        })
+        var c = new Reve({
+            data: function () {
+                return {
+                    num: 10
+                }
+            },
+            template: '<img r-num="{num + 20}" />'
+        })
+        c.$update()
+        assert(inited)
+    })
+    it('directive-expression:function', function () {
+        var inited = false
+        Reve.directive('func', {
+            bind: function (v, expr) {
+                inited = true
+                assert.equal(v, 20)
+                assert.equal(expr, 'add(num, 10)')
+            },
+            update: function (v) {
+                assert.equal(v, 20)
+            }
+        })
+        var c = new Reve({
+            data: function () {
+                return {
+                    num: 10
+                }
+            },
+            template: '<img r-func="{add(num, 10)}" />',
+            methods: {
+                add: function (num, addNum) {
+                    return num + addNum
+                }
+            }
+        })
+        c.$update()
+        assert(inited)
+    })
     it('directive-expression:string', function () {
         var inited = false
         var updateTimes = 0
@@ -61,5 +112,23 @@ describe('Global API', function () {
         c.$update()
         assert(inited)
         assert(updateTimes, 1)
+    })
+    it('directive-expression:empty', function () {
+        var inited = false
+        Reve.directive('empty', {
+            bind: function (v, expr) {
+                assert.equal(v, '')
+                assert.equal(expr, '')
+            },
+            update: function (v) {
+                inited = true
+                assert.equal(v, '')
+            }
+        })
+        var c = new Reve({
+            template: '<img r-empty="" />'
+        })
+        c.$update()
+        assert(inited)
     })
 })
