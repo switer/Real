8109c0a170025226bdcdb1cf7d0443fcd17a750b
switer <guankaishe@gmail.com>
2015-12-04 20:56:45 +0800

    1. update doc; 2. class method of component test case

--- test/spec-classmethod.js
+++ test/spec-classmethod.js
@@ -9,17 +9,45 @@ describe('# Class Methods', function () {
             }
         })
         var c = new C({
+            el: document.createElement('div'),
             data: function () {
                 return {
                     name: 'real',
                     na: 'china'
                 }
-            },
-            el: document.createElement('div')
+            }
         })
-
         assert.equal(c.$data.name, 'real')
         assert.equal(c.$data.na, 'china')
         assert.equal(c.$data.address, 'none')
+
+        var c2 = new C({
+            el: document.createElement('div'),
+            data: {
+                name: 'real2'
+            }
+        })
+        assert.equal(c2.$data.name, 'real2')
+        assert.equal(c2.$data.address, 'none')
+    })
+
+    it('component', function (){
+        var C = Reve.component('custom-component', {
+            template: '<div class="custom-component"></div>',
+            data: function () {
+                return {
+                    name: 'custom-component'
+                }
+            }
+        })
+        var c = new C({
+            el: document.createElement('div')
+        })
+        assert(!!c.$el.querySelector('.custom-component'))
+        var c2 = new Reve({
+            el: document.createElement('div'),
+            template: '<div r-component="custom-component"></div>'
+        })
+        assert(!!c2.$el.querySelector('.custom-component'))
     })
 })
