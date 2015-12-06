1dd6f390c022ad62613d1c2a378848c576f3f149
switer <guankaishe@gmail.com>
2015-12-06 16:52:17 +0800

    instance method test case

---
+++ test/spec-instancemethod.js
@@ -0,0 +1,28 @@
+describe('# Instance Methods', function () {
+    it('$set', function () {
+        var c = new Reve({
+            template: '<div r-text="replace">{name}</div>',
+            data: {
+                name: ''
+            }
+        })
+        c.$set('name', 'real')
+        assert.equal(c.$data.name, 'real')
+        assert.equal(c.$el.innerText, 'real')
+    })
+    it('$compile', function () {
+        var c = new Reve({
+            template: '<div class="con"></div>',
+            data: {
+                name: 'real'
+            }
+        })
+        var inserted = '<div r-text="replace">{name}</div>'
+        var el = c.$compile(inserted)
+        assert.equal(el.nodeType, 11)
+        
+        var con = c.$el.querySelector('.con')
+        con.appendChild(el)
+        assert.equal(con.innerText, 'real')
+    })
+})
